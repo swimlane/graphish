67614e872962f5b501fb64557f6720ebbaf0dbee
Josh Rickard <josh.rickard@swimlane.com>
2019-07-25 10:55:58 -0500

    Added ability to search all discovered mailbox users

--- graphish/search.py
+++ graphish/search.py
@@ -1,11 +1,12 @@
 from message import Message
+from users import Users
 
 
 class Search(object):
     """
-    This class allows you to interact with and search mailboxes using Microsoft Graph API.  It's parent class is GraphConnector which handles all authentication.
+    This class allows you to interact with and search mailboxes using Microsoft Graph API.
     """
-    def __init__(self, graphConnector, includeNestedFolders=True, userPrincipalName='me'):
+    def __init__(self, graphConnector, includeNestedFolders=True, userPrincipalName=None):
         """The Search class allows you to:
            * Create a new Search
            * Update an existing Search
@@ -19,12 +20,22 @@ class Search(object):
             userPrincipalName (str, optional): Defaults to the current user, but can be any user defined or provided in this parameter. Defaults to 'me'.
         """
         self.connector = graphConnector
-        if userPrincipalName is not 'me':
-            self.user = 'users/%s' % userPrincipalName
+        if not userPrincipalName:
+            if self.connector.username and self.connector.password:
+                self.user = 'me'
+            else:
+                self.user = Users(self.connector).get
         else:
-            self.user = userPrincipalName
+            self.user = 'users/%s' % userPrincipalName
+        
 
         self.includeNestedFolders = includeNestedFolders
+
+    def _get_folder_id(self, user):
+        for folder in self.folderId:
+            for key, val in folder.iteritems():
+                if user == key:
+                    return val
 
     @property
     def folderId(self):
@@ -35,54 +46,134 @@ class Search(object):
         self._folderId = value
 
     def folders(self):
-        uri = '%s/mailFolders/%s' % (self.user, self.folderId)
-        return self.connector.invoke('GET', uri).json()
+        return_list = []
+        if isinstance(self.user, list):
+            for user in self.user:
+                folder_id = self._get_folder_id(user)
+                uri = 'users/%s/mailFolders/%s' % (user, folder_id)
+                return_list.append({user: self.connector.invoke('GET', uri).json()})
+            return return_list
+        else:
+            folder_id = self._get_folder_id(self.user)
+            uri = '%s/mailFolders/%s' % (self.user, folder_id)
+            return [{self.user: self.connector.invoke('GET', uri).json()}]
 
     def messages(self):
-        uri = '%s/mailFolders/%s/messages' % (self.user, self.folderId)
-        response = self.connector.invoke('GET', uri).json()
-        message_list = []
-        for message in response['value']:
-            message_list.append(
-                Message(
-                    self.connector,
-                    message,
-                    self.user
+        return_list = []
+        #folder_id_list = []
+        if isinstance(self.user, list):
+            for user in self.user:
+                folder_id = self._get_folder_id(user)
+                uri = 'users/%s/mailFolders/%s/messages' % (user, folder_id)
+                response = self.connector.invoke('GET', uri).json()
+                message_list = []
+                for message in response['value']:
+                    message_list.append(
+                        Message(
+                            self.connector,
+                            message,
+                            self.user
+                        )
+                    )
+                return_list.append({
+                    user: message_list
+                })
+            return return_list
+        else:
+            folder_id = self._get_folder_id(self.user)
+            uri = '%s/mailFolders/%s/messages' % (self.user, folder_id)
+            response = self.connector.invoke('GET', uri).json()
+            message_list = []
+            for message in response['value']:
+                message_list.append(
+                    Message(
+                        self.connector,
+                        message,
+                        self.user
+                    )
                 )
-            )
-        return message_list
+            return [{self.user: message_list}]
 
     def create(self, searchFolderName, sourceFolder, filterQuery):
-        uri = '%s/mailFolders/%s/childFolders' % (self.user, sourceFolder)
-        body = {
-            "@odata.type": "microsoft.graph.mailSearchFolder",
-            "displayName": "%s" % searchFolderName,
-            "includeNestedFolders": '%s' % self.includeNestedFolders,
-            "sourceFolderIds": ["%s" % sourceFolder],
-            "filterQuery": filterQuery
-        }
-        response = self.connector.invoke('POST', uri, data=body)
-        self.folderId = response.json()['id']
-        return response.json()
+        return_list = []
+        folder_id_list = []
+        if isinstance(self.user, list):
+            for user in self.user:
+                uri = 'users/%s/mailFolders/%s/childFolders' % (user, sourceFolder)
+                body = {
+                    "@odata.type": "microsoft.graph.mailSearchFolder",
+                    "displayName": "%s" % searchFolderName,
+                    "includeNestedFolders": '%s' % self.includeNestedFolders,
+                    "sourceFolderIds": ["%s" % sourceFolder],
+                    "filterQuery": filterQuery
+                }
+                response = self.connector.invoke('POST', uri, data=body)
+                return_list.append({user: response.json()})
+                folder_id_list.append({user: response.json()['id']})
+            self.folderId = folder_id_list
+            return return_list
+        else:
+            uri = '%s/mailFolders/%s/childFolders' % (self.user, sourceFolder)
+            body = {
+                "@odata.type": "microsoft.graph.mailSearchFolder",
+                "displayName": "%s" % searchFolderName,
+                "includeNestedFolders": '%s' % self.includeNestedFolders,
+                "sourceFolderIds": ["%s" % sourceFolder],
+                "filterQuery": filterQuery
+            }
+            response = self.connector.invoke('POST', uri, data=body)
+            self.folderId = [{self.user: response.json()['id']}]
+            return [response.json()]
 
     def update(self, searchFolderName=None, sourceFolder=None, filterQuery=None):
-        uri = '%s/mailFolders/%s' % (self.user, self.folderId)
-        body = {}
-        body['@odata.type'] = "microsoft.graph.mailSearchFolder"
-        
-        if searchFolderName:
-            body['displayName'] = searchFolderName
-        if sourceFolder:
-            body['sourceFolderIds'] = sourceFolder
-        if filterQuery:
-            body['filterQuery'] = filterQuery
-        body['includeNestedFolders'] ='%s' % self.includeNestedFolders
+        return_list = []
+        folder_id_list = []
+        if isinstance(self.user, list):
+            for user in self.user:
+                uri = 'users/%s/mailFolders/%s' % (user, self._get_folder_id(user))
+                body = {}
+                body['@odata.type'] = "microsoft.graph.mailSearchFolder"
+                
+                if searchFolderName:
+                    body['displayName'] = searchFolderName
+                if sourceFolder:
+                    body['sourceFolderIds'] = sourceFolder
+                if filterQuery:
+                    body['filterQuery'] = filterQuery
+                body['includeNestedFolders'] ='%s' % self.includeNestedFolders
 
-        response = self.connector.invoke('POST', uri, data=body)
-        self.folderId = response.json()['id']
-        return response.json()
+                response = self.connector.invoke('POST', uri, data=body)
+                folder_id_list.append({user: response.json()['id']})
+                return_list.append({user: response.json()})
+            self.folderId = folder_id_list
+            return return_list
+        else:
+            uri = '%s/mailFolders/%s' % (self.user, self._get_folder_id(self.user))
+            body = {}
+            body['@odata.type'] = "microsoft.graph.mailSearchFolder"
+            
+            if searchFolderName:
+                body['displayName'] = searchFolderName
+            if sourceFolder:
+                body['sourceFolderIds'] = sourceFolder
+            if filterQuery:
+                body['filterQuery'] = filterQuery
+            body['includeNestedFolders'] ='%s' % self.includeNestedFolders
+
+            response = self.connector.invoke('POST', uri, data=body)
+            self.folderId = [{self.user: response.json()['id']}]
+            return [response.json()]
+
 
     def delete(self):
-        uri = '%s/mailFolders/%s' % (self.user, self.folderId)
-        response = self.connector.invoke('DELETE', uri)
-        return response.json()
+        return_list = []
+        if isinstance(self.user, list):
+            for user in self.user:
+                uri = 'users/%s/mailFolders/%s' % (user, self._get_folder_id(user))
+                response = self.connector.invoke('DELETE', uri)
+                return_list.append({user: response.json()})
+            return return_list
+        else:
+            uri = '%s/mailFolders/%s' % (self.user, self._get_folder_id(self.user))
+            response = self.connector.invoke('DELETE', uri)
+            return [{self.user: response.json()}]
