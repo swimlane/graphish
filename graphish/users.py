96f815d1a6f27a0d63a25f45045b503d0dde22c4
Josh Rickard <josh.rickard@swimlane.com>
2019-07-25 10:56:09 -0500

    Added class to get all users in Azure AD

---
+++ graphish/users.py
@@ -0,0 +1,25 @@
+
+
+class Users(object):
+    """
+    This class allows you to retrieve all users within your organization.  Your Azure AD application will need to have the User.Read.All permissions granted for this application.
+    """
+    def __init__(self, graphConnector):
+        """The Users class allows you to:
+           * Get all email address within your Azure tenant
+        
+        Args:
+            graphConnector (GraphConnector): A generated GraphConnector object
+        """
+        self.connector = graphConnector
+
+    @property
+    def get(self):
+        return_list = []
+        uri = 'users'
+        users = self.connector.invoke('GET', uri).json()
+        for user in users['value']:
+            # Adding logic hear to attempt to catch mailboxes not in Exchange Online
+            if user['userPrincipalName'] == user['mail']:
+                return_list.append(user['userPrincipalName'])
+        return return_list
